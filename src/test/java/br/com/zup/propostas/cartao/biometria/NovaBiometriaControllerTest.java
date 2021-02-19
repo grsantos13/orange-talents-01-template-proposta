ac86dbcb77deb6954664ee50cad861e638c6a2db
Gustavo Santos <gsantoset@gmail.com>
2021-02-19 14:34:42 -0300

    Incluído Prometheus.yml.

--- src/test/java/br/com/zup/propostas/cartao/biometria/NovaBiometriaControllerTest.java
+++ src/test/java/br/com/zup/propostas/cartao/biometria/NovaBiometriaControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
-import java.util.Base64;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
